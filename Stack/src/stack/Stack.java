e830a6c1142ab9ab87975e411f2b31e9e6f5c8e1
moisesfdasilva <msesfernandes@gmail.com>
2025-10-23 21:33:18 -0300

    first commit

---
+++ Stack/src/stack/Stack.java
@@ -0,0 +1,22 @@
+package stack;
+
+/**
+ *
+ * @author msilva
+ */
+public class Stack {
+
+    /**
+     * @param args the command line arguments
+     * Method to run the stack
+     */
+    public static void main(String[] args) {
+        System.out.println("ABC");
+    }
+    
+    private class Node {
+        public int number;
+        public Node node;
+    }
+        
+}
